249c5718914fcbf6ca53d1924f2bac75ba7c9dc6
lilia <liliakai@gmail.com>
2013-09-12 20:09:36 -0700

    Add inverse of hex_to_str and fix bugs, comments, etc

--- util.rb
+++ util.rb
@@ -1,14 +1,20 @@
-/* converts a hexidecimal string to a string ex: "68656c6c6f" -> "hello" */
-def hex_to_str(h):
+# converts a hexidecimal string to a string ex: "68656c6c6f" -> "hello"
+def hex_to_str(h)
   [h].pack('H*')
 end
 
-/* returns a string which is the byte-wise xor of two strings, s1 and s2 */
-def str_xor(s1, s2):
+# converts a string to a hexadecimal string ex: "hello" -> "68656c6c6f"
+def str_to_hex(s)
+  s.bytes.map{ |b| b.to_s(16) }.join
+end
+
+# returns a string which is the byte-wise xor of two strings, s1 and s2
+def str_xor(s1, s2)
   if s1.length != s2.length:
-    maxlen = [s1, s2].map(&length).max
-    s1 = s1[0...maxlen]
-    s2 = s2[0...maxlen]
+    minlen = [s1, s2].map(&:length).min
+    s1 = s1[0...minlen]
+    s2 = s2[0...minlen]
+    puts minlen
   end
   s1.bytes.zip(s2.bytes).map{ |b1, b2| b1 ^ b2 }.map{ |b| b.chr }.join
 end
